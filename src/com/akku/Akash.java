8af08ba0b7165ff49d36b43cf3fb44b7a220f74e
Akash-Mondal2004 <aakashmondal43@gmail.com>
2026-03-09 12:25:01 +0530

    change

--- src/com/akku/Akash.java
+++ src/com/akku/Akash.java
@@ -1,11 +1,14 @@
 package com.akku;
-
+import java.util.*;
 public class Akash {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "Samik takes Arka's stick";
 		System.out.println(str);
+		Scanner sc = new Scanner("System.out");
+		int n = sc.nextInt();
+		System.out.println(n);
 	}
 
 }

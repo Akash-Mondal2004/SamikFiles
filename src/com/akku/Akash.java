2b105582f4c51f6f41a7382cc9fe817256bfc478
Akash-Mondal2004 <aakashmondal43@gmail.com>
2026-03-09 12:17:09 +0530

    Samik or Arka ki prem kahani

---
+++ src/com/akku/Akash.java
@@ -0,0 +1,11 @@
+package com.akku;
+
+public class Akash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Samik takes Arka's stick";
+		System.out.println(str);
+	}
+
+}
